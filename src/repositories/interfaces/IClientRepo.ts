e8933739853fb5c9a5423e373cd9517fb4eda3ea
Luís Lira <1171246@isep.ipp.pt>
2019-11-29 10:20:11 +0000

    Client Repository Pattern implementation started, opens #1

---
+++ src/repositories/interfaces/IClientRepo.ts
@@ -0,0 +1,7 @@
+export interface IClientRepo {
+  /*  findUserByEmail(email: UserEmail): Promise<User>;
+  findUserByUsername (username: string): Promise<User>;
+  exists (email: UserEmail): Promise<boolean>;
+  save(user: User): Promise<void>;*/
+  doSomethingHere();
+}
