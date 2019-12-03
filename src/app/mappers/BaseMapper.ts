415179d701b9c22977b1cf813708ccfaca9d40cd
Guilherme Oliveira <1170814@isep.ipp.pt>
2019-12-03 22:46:19 +0000

    Client Mapper [Implementation] [refs #2]

---
+++ src/app/mappers/BaseMapper.ts
@@ -0,0 +1,5 @@
+export abstract class BaseMapper<Model> {
+    //public static toPersistence(model: T): any {}
+    //public static toDomain(raw: any): T {}
+    //public static toDTO(model: T): E {}
+}
