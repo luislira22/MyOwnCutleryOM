aedf249cc3a082a8e94450e1272022454c62f065
Guilherme Oliveira <1170814@isep.ipp.pt>
2019-12-05 17:35:26 +0000

    Update Client (name, address)  [Implemention] [refs #9]

--- src/app/services/ClientService.ts
+++ src/app/services/ClientService.ts
@@ -29,9 +29,24 @@ class ClientService implements IClientService {
         throw new Error("not implemented")
     }
 
+    updateNameAndAddress(_id: string, item: ClientDTO, callback: (error: any, result: any) => void ) {
+        this._clientRepository.findById(_id, (err, res) => {
+            if (err) callback(err, res);
+            else {
+                if(item.address.address == undefined || item.address.postalcode == undefined || item.address.city == undefined || item.address.country == undefined ||
+                    item.name.firstname == undefined || item.name.lastname == undefined ) {} //throw new Error('address and name must be defined'); //TODO
+                //Email
+                item.email = res.email[0].email;
+                //Password
+                item.password = res.password;
+                let client = ClientMapper.toDomain(item);
+                this._clientRepository.update(res._id, client, callback);
+            }
+        })
+    }
+
     delete(_id: string, callback: (error: any, result: any) => void) {
-        //this._clientRepository.delete(_id, callback);
-        throw new Error("not implemented")
+        this._clientRepository.delete(_id, callback);
     }
 
     findById(_id: string, callback: (error: any, result: any) => void) {
