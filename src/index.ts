32e9ea838bf071ace4a6992ebb8d2237e54b9609
Tomás da Silva Soares <1170635@isep.ipp.pt>
2019-12-06 16:24:23 +0000

    Cors Policy fix [closes #12 ]

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
     import express = require("express");
+    import cors = require("cors")
     import Middlewares = require("./config/middlewares/base/MiddlewaresBase");
     
     var app = express();
+
+    app.use(cors())
+
     var port = parseInt(process.env.PORT, 10) || 5000;
     app.set("port", port);
     app.use(Middlewares.configuration);
