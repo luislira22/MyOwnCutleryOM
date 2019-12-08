f6ea0071280050583c15c57c9f352f96124a4db8
Guilherme Oliveira <1170814@isep.ipp.pt>
2019-12-08 18:35:47 +0000

    Client Unit Tests [Testing] [refs #3]

---
+++ tests/model/clients/NIF.test.ts
@@ -0,0 +1,36 @@
+import NIF from "../../../src/app/model/clients2/NIF";
+
+test('Create a valid nif', () => {
+    new NIF(123456789);
+});
+
+describe('Create an invalid nif', () => {
+    it('with more than 9 number', () => {
+        try {
+            new NIF(111111111111111);
+            expect(true).toBe(false);
+        }
+        catch(e) {
+            expect(e.message).toBe('NIF must be correct.');
+        }
+    });
+    it('with less than 9 number', () => {
+        try {
+            new NIF(1111);
+            expect(true).toBe(false);
+        }
+        catch(e) {
+            expect(e.message).toBe('NIF must be correct.');
+        }
+    });
+    it('with negative number', () => {
+        try {
+            new NIF(-1);
+            expect(true).toBe(false);
+        }
+        catch(e) {
+            expect(e.message).toBe('NIF must be correct.');
+        }
+    });
+});
+
---
+++ tests/model/clients/address.test.ts
@@ -0,0 +1,75 @@
+import Address from "../../../src/app/model/clients2/Address";
+
+describe('Create a valid address', () => {
+    let address = new Address('Rua da Avenida', '4010-200', 'Porto', 'Portugal')
+    it('Address correct', () => {
+        expect(address.address).toBe('Rua da Avenida')
+    });
+    it('Postalcode correct', () => {
+        expect(address.postalcode).toBe('4010-200')
+    });
+    it('City correct', () => {
+        expect(address.city).toBe('Porto')
+    });
+    it('Country correct', () => {
+        expect(address.country).toBe('Portugal')
+    });
+});
+
+describe('Create an invalid address', () => {
+    it('empty address', () => {
+        try {
+            new Address('', '4010-200', 'Porto', 'Portugal')
+            expect(true).toBe(false)
+        }
+        catch (e) {
+            expect(e.message).toBe('Address, city, postalcode and country must be filled.')
+        }
+    });
+    it('empty postalcode', () => {
+        try {
+            new Address('Rua da Avenida', '', 'Porto', 'Portugal')
+            expect(true).toBe(false)
+        }
+        catch (e) {
+            expect(e.message).toBe('Address, city, postalcode and country must be filled.')
+        }
+    });
+    it('empty city', () => {
+        try {
+            new Address('Rua da Avenida', '4010-200', '', 'Portugal')
+            expect(true).toBe(false)
+        }
+        catch (e) {
+            expect(e.message).toBe('Address, city, postalcode and country must be filled.')
+        }
+    });
+    it('empty country', () => {
+        try {
+            new Address('Rua da Avenida', '4010-200', 'Porto', '')
+            expect(true).toBe(false)
+        }
+        catch (e) {
+            expect(e.message).toBe('Address, city, postalcode and country must be filled.')
+
+        }
+    });
+    it('address should be greater than 1', () => {
+        try {
+            new Address('R', '4010-200', 'Porto', 'Portugal')
+            expect(true).toBe(false)
+        }
+        catch (e) {
+            expect(e.message).toBe('Address, city, postalcode and country must be filled.')
+        }
+    });
+    it('address should not be undefined', () => {
+        try {
+            new Address(null, '4010-200', 'Porto', 'Portugal')
+            expect(true).toBe(false)
+        }
+        catch (e) {
+            expect(e.message).toBe('Address, city, postalcode and country must be filled.')
+        }
+    });
+});
